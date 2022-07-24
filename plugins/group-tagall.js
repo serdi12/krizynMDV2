48dbaa0f612cca57d6b648c49d6e2781d5435e47
serdi12 <109755111+serdi12@users.noreply.github.com>
2022-07-24 08:39:27 +0700

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, participants }) => {
 		      	for (let mem of participants) {
 		            teks += ` ㇱ @${mem.id.split('@')[0]}\n`
 				}
-                teks += `\n↻𝐀𝐫𝐮𝐥𝐥𝐎𝐟𝐜`
+                teks += `\n↻𝚒𝚢𝚊𝚗`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
